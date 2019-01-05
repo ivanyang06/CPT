91cfd3de642ac32872f5380e83e80d24c835e335
ivanyang06 <43610226+ivanyang06@users.noreply.github.com>
2019-01-04 21:18:36 -0500

    Update and rename obstacle.pyde to obstacle.py

--- obstacle.py
+++ obstacle.py
@@ -1,9 +1,9 @@
 class Obstacle(object):
-    xkey = false
-    ykey = false
+    xkey = False
+    ykey = False
     speedX = 0
     speedY = 0
-    src = "Red.png"
+    src = "red.png"
     type = "blank"
     counter = 0
     lastDir = "up"
@@ -19,24 +19,24 @@ class Obstacle(object):
         self.movetype = movetype 
    
 
-    def setSource(String source):
+    def setSource(source):
         src = source 
    
 
-    def move():
+    def move(self):
         if True: 
-            x += speedX 
+            self.x += self.speedX 
      
         if True:  
-            y += speedY 
+            self.y += self.speedY 
      
         if self.movetype == "movingenemy":
-            self.x += direction 
+            self.x += self.direction 
      
    
 
     def animate():
-
+        print("animate called")
    
     animate()
     
@@ -50,13 +50,13 @@ class Obstacle(object):
         if ((speedX == 0 and speedY == 0) or (counter == 22)):  
             counter = 0 
         else:  
-            counter++ 
+            counter+=1
      
         if Game.tora.x + Game.Camera.x < self.x + self.width - 3 and Game.tora.x + Game.tora.width + Game.Camera.x > self.x + 3: 
           if Game.tora.y + Game.Camera.y <= self.y + self.height - 2and Game.tora.y + Game.tora.height + Game.Camera.y >= self.y + 10: 
             if self.type == "wall":  
                 Game.tora.y += 3 
-                Game.tora.up = false 
+                Game.tora.up = False 
                 Game.tora.loseHealth(2, 100) 
             
             if self.type == "urchon":
@@ -72,7 +72,7 @@ class Obstacle(object):
         if Game.tora.y + Game.Camera.y <= self.y + self.height - 10 and Game.tora.y + Game.Camera.y + Game.tora.height >= self.y + 2: 
             if self.type == "wall":  
                 Game.tora.y -= 3 
-                Game.tora.down = false 
+                Game.tora.down = False 
                 Game.tora.loseHealth(1, 100) 
                 
             if self.type == "urchon":
@@ -90,7 +90,7 @@ class Obstacle(object):
             if Game.tora.x + Game.Camera.x <= self.x + self.width - 2 and Game.tora.x + Game.Camera.x + Game.tora.width >= self.x + 10:
                 if self.type == "wall": 
                     Game.tora.x += 3 
-                    Game.tora.left = false 
+                    Game.tora.left = False 
                     Game.tora.loseHealth(1, 100) 
                 
                 if self.type == "urchon":  
@@ -106,7 +106,7 @@ class Obstacle(object):
         if Game.tora.x + Game.Camera.x <= self.x + self.width - 10 and Game.tora.x + Game.Camera.x + Game.tora.width >= self.x + 2:  
             if self.type == "wall":  
                 Game.tora.x -= 3 
-                Game.tora.right = false 
+                Game.tora.right = False 
                 Game.tora.loseHealth(1, 100) 
             
             if self.type == "urchon":  
@@ -121,7 +121,7 @@ class Obstacle(object):
         
         
         if self.movetype == "movingenemy":
-            if Game.weapon.attack == true and Game.weapon.x <= self.x + self.width and Game.weapon.x + Game.weapon.width >= self.x and Game.weapon.y + Game.Camera.y <= self.y + self.width and Game.weapon.y + Game.weapon.height + Game.Camera.y >= self.y):  
+            if Game.weapon.attack == True and Game.weapon.x <= self.x + self.width and Game.weapon.x + Game.weapon.width >= self.x and Game.weapon.y + Game.Camera.y <= self.y + self.width and Game.weapon.y + Game.weapon.height + Game.Camera.y >= self.y:  
                 if (self.type == "aligo"):
                     print("hit") 
                     Game.obstacles.remove(self) 
@@ -137,45 +137,21 @@ class Obstacle(object):
             if Game.obstacles.get(i).type == "wall":
                 if self.y < Game.obstacles.get(i).y + Game.obstacles.get(i).height and self.y + self.width > Game.obstacles.get(i).y:
                     if direction > 0 and self.x + self.width + 10 > Game.obstacles.get(i).x and self.x + self.width + 10 < Game.obstacles.get(i).x + Game.obstacles.get(i).width:
-                    direction *= -1 
-                    break 
+                        direction *= -1 
+                        break 
                 
                 if direction < 0 and self.x - 10 > Game.obstacles.get(i).x and self.x - 10 < Game.obstacles.get(i).x + Game.obstacles.get(i).width:  
                     direction *= -1 
                     break 
                 
 
-    def paint(Graphics2D g):
-        ImageIcon picture = new ImageIcon(src) 
-        Image MyCharacter = picture.getImage() 
-        g.drawImage(MyCharacter, x, y, width, height, null) 
+    def paint(self):
+        loadedimage = loadImage(self.src)
+        image(loadedimage,self.x,self.y,self.width,self.height)
     
     
-    def keyReleased(KeyEvent e):
+    def keyReleased(e):
         if e.getKeyCode() == KeyEvent.VK_LEFT or e.getKeyCode() == KeyEvent.VK_RIGHT: 
-            xkey = false 
+            xkey = False 
         else:
-            ykey = false 
-
-     
-   
-
-  public void keyPressed(KeyEvent e)  
-    if (e.getKeyCode() == KeyEvent.VK_LEFT)  
-      xkey = true 
-      speedX = 1 
-     
-    if (e.getKeyCode() == KeyEvent.VK_RIGHT)  
-      xkey = true 
-      speedX = -1 
-     
-    if (e.getKeyCode() == KeyEvent.VK_UP)  
-      ykey = true 
-      speedY = 1 
-     
-    if (e.getKeyCode() == KeyEvent.VK_DOWN)  
-      ykey = true 
-      speedY = -1 
-     
-   
- 
+            ykey = False 
