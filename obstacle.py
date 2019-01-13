7bd93609a87e0d74aae971767ed9ad6930632226
ivanyang06 <43610226+ivanyang06@users.noreply.github.com>
2019-01-13 16:53:12 -0500

    Update obstacle.py

--- obstacle.py
+++ obstacle.py
@@ -41,7 +41,7 @@ class Obstacle(object):
    
     animate()
     
-    def update(self, tora, Camera): 
+    def update(self, tora, Camera, obstacles, weapon): 
         if self.xkey == False: 
             self.speedX = 0 
      
@@ -54,9 +54,9 @@ class Obstacle(object):
             self.counter+=1
      
         if tora.x + Camera.x < self.x + self.width - 3 and tora.x + tora.width + Camera.x > self.x + 3: 
-          if tora.y + Camera.y <= self.y + self.height - 2 and tora.y + tora.height + Camera.y >= self.y + 10: 
+          if tora.y + Camera.y <= self.y + self.height - 3 and tora.y + tora.height + Camera.y >= self.y + 10: 
             if self.type == "wall":  
-                tora.y += 4
+                tora.y += 5
                 tora.up = False 
                 tora.loseHealth(2, 100) 
             
@@ -70,9 +70,9 @@ class Obstacle(object):
                 tora.loseHealth(3, 100) 
             
         
-          if tora.y + Camera.y <= self.y + self.height - 10 and tora.y + Camera.y + tora.height >= self.y + 2: 
+          if tora.y + Camera.y <= self.y + self.height - 10 and tora.y + Camera.y + tora.height >= self.y + 3: 
             if self.type == "wall":  
-                tora.y -= 4
+                tora.y -= 5
                 tora.down = False 
                 tora.loseHealth(1, 100) 
                 
@@ -88,9 +88,9 @@ class Obstacle(object):
         
         
         if tora.y + Camera.y < self.y + self.height - 3 and tora.y + tora.height + Camera.y > self.y + 3:  
-            if tora.x + Camera.x <= self.x + self.width - 2 and tora.x + Camera.x + tora.width >= self.x + 10:
+            if tora.x + Camera.x <= self.x + self.width - 3 and tora.x + Camera.x + tora.width >= self.x + 10:
                 if self.type == "wall": 
-                    tora.x += 4
+                    tora.x += 3
                     tora.left = False 
                     tora.loseHealth(1, 100) 
                 
@@ -104,9 +104,9 @@ class Obstacle(object):
                     tora.loseHealth(3, 100) 
             
         
-            if tora.x + Camera.x <= self.x + self.width - 10 and tora.x + Camera.x + tora.width >= self.x + 2:  
+            if tora.x + Camera.x <= self.x + self.width - 10 and tora.x + Camera.x + tora.width >= self.x + 3:  
                 if self.type == "wall":  
-                    tora.x -= 4
+                    tora.x -= 3
                     tora.right = False 
                     tora.loseHealth(1, 100) 
             
@@ -121,29 +121,28 @@ class Obstacle(object):
             
         
         
-#        if self.movetype == "movingenemy":
-#            if Game.weapon.attack == True and Game.weapon.x <= self.x + self.width and Game.weapon.x + Game.weapon.width >= self.x and Game.weapon.y + Game.Camera.y <= self.y + self.width and Game.weapon.y + Game.weapon.height + Game.Camera.y >= self.y:  
-#                if (self.type == "aligo"):
-#                    print("hit") 
-#                    Game.obstacles.remove(self) 
-#                elif self.type == "turtle" and Game.tora.attackDir == "up":  
-#                    print("hit") 
-#                    Game.obstacles.remove(self) 
-#            
-#        
-#        if direction > 0 and self.x + self.width + 10 > 512 or direction < 0 and self.x - 10 < 0: 
-#            direction *= -1 
-#        
-#        for i in Game.obstacles:
-#            if Game.obstacles.get(i).type == "wall":
-#                if self.y < Game.obstacles.get(i).y + Game.obstacles.get(i).height and self.y + self.width > Game.obstacles.get(i).y:
-#                    if direction > 0 and self.x + self.width + 10 > Game.obstacles.get(i).x and self.x + self.width + 10 < Game.obstacles.get(i).x + Game.obstacles.get(i).width:
-#                        direction *= -1 
-#                        break 
-#                
-#                if direction < 0 and self.x - 10 > Game.obstacles.get(i).x and self.x - 10 < Game.obstacles.get(i).x + Game.obstacles.get(i).width:  
-#                    direction *= -1 
-#                    break 
+        if self.movetype == "movingenemy":
+            if weapon.attack == True and weapon.x <= self.x + self.width and weapon.x + weapon.width >= self.x and weapon.y + Camera.y <= self.y + self.width and weapon.y + weapon.height + Camera.y >= self.y:  
+                if (self.type == "aligo"):
+                    print("hit") 
+                    obstacles.remove(self) 
+                elif self.type == "turtle" and tora.attackDir == "up":  
+                    print("hit") 
+                    obstacles.remove(self) 
+            
+        
+        if self.direction > 0 and self.x + self.width + 10 > 512 or self.direction < 0 and self.x - 10 < 0: 
+            self.direction *= -1 
+        
+        for i in obstacles:
+            if i.type == "wall":
+                if self.y < i.y + i.height and self.y + self.width > i.y:
+                    if self.direction > 0 and self.x + self.width + 10 > i.x and self.x + self.width + 10 < i.x + i.width:
+                        self.direction *= -1 
+                        break             
+                    if self.direction < 0 and self.x - 10 > i.x and self.x - 10 < i.x + i.width:  
+                        self.direction *= -1 
+                        break 
 
     def paint(self):
         image(self.loadedimage,self.x,self.y,self.width,self.height)
